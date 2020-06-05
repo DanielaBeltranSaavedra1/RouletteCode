7054f1c0159b6244ac214a86e55facddfc290959
Daniela <dpbeltran97@gmail.com>
2020-06-04 20:40:45 -0500

    Init project

---
+++ src/main/java/com/casino/roulette/roulette_casino/RouletteCasinoApplication.java
@@ -0,0 +1,13 @@
+package com.casino.roulette.roulette_casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteCasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteCasinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casino/roulette/roulette_casino/RouletteCasinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casino.roulette.roulette_casino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteCasinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
